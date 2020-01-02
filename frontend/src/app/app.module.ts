e902230107946e88317165f84a4eede04d7371dd
Seif Hatem <seifhatem@seifs-mbp-2.lan>
2020-01-02 12:43:55 +0200

    Adding a task in Angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ViewListComponent } from './viewList/view-list.component';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http'
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/viewList/view-list.component.ts
+++ frontend/src/app/viewList/view-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient,HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -14,14 +14,43 @@ import { Injectable } from '@angular/core';
 
 export class ViewListComponent implements OnInit{
 
+
+
 constructor(private http: HttpClient) { }
 
 list = [];
+newTaskTitle = "";
 
-ngOnInit() {
-this.http.get("http://localhost:3000/list").subscribe((res)=>{
-            this.list = res;
-            console.log(this.list);
-        });
-  }
+
+onAddClick(){
+if(this.newTaskTitle.length>1){
+  alert("adding "+ this.newTaskTitle + "to the list");
+
+
+let body = new URLSearchParams();
+body.set('title', this.newTaskTitle);
+
+let options = {
+    headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+};
+
+  this.http.post("http://localhost:3000/add", body.toString(),options,{responseType: 'text'})
+          .subscribe(data => {
+              this.reloadList();
+          }, error => {
+              alert("Error updating the list");
+          });
+
 }
+}
+
+reloadList(){
+  this.http.get("http://localhost:3000/list").subscribe((res)=>{
+              this.list = res;
+          });
+    }
+    ngOnInit() {
+    this.reloadList()
+    }
+
+  }
